fa9cd9189e7d7a2cdcbf4e301861fe02976469ba
laponte16 <54826992+laponte16@users.noreply.github.com>
2023-02-07 17:36:11 -0400

    Update Home.js

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -16,12 +16,17 @@ function Home() {
             </div>
             <div className='w-full fondo-gray mt-8 rounded pt-1 pb-8 pl-4 texto-white'>
                 <p className='texto-subtitulo'><b>Who We Are</b></p>
-                <p className='w-4/5 mt-1'><b>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent a est ipsum. Duis posuere purus nec feugiat ullamcorper. Pellentesque viverra nisl eget magna rhoncus, at iaculis sapien lacinia. Nam pharetra, enim finibus egestas rutrum, enim nulla elementum nisi, quis scelerisque nunc metus eget ante. Aenean iaculis, augue vel porta laoreet, dolor ligula ornare diam, et convallis arcu elit quis urna. Integer sollicitudin sagittis tortor eget vestibulum. Cras sed justo ultrices, tincidunt urna a, sollicitudin purus.</b></p>
+                
+                <p className='w-4/5 mt-1'>Welcome all!</p>
+                <br></br>
+                <p>Thunderbit Code is an independent group of Computer Engineers specialized in Web Development, offering services ranging from simple static pages to the most state-of-the-art Apps. We're passionate about our work and trying out new things everyday, firmly bilinguals in both English and Spanish and fielding full fledged modern design and marketing strategies.</p>
+                <br></br>
+                <p>If you're starting a new business, branching out to new industries, modernizing your brand or simply want a little piece of the internet to build your dreams on? We would be more than happy to work with you together on achieving it.</p>
             </div>
-            <div className='flex flex-row justify-center'>
-                <img src={logo} className='w-1/5 p-2'></img>
-                <img src={logo} className='w-1/5 p-2'></img>
-                <img src={logo} className='w-1/5 p-2'></img>
+            <div className='flex flex-row justify-center w-full mt-10 px-16'>
+                <div className='flex flex-col justify-end w-1/4 p-2'><img src={logo}></img></div>
+                <div className='w-2/4 flex flex-col justify-end p-2'><img src={logo}></img></div>
+                <div className='w-1/4 flex flex-col justify-end p-2'><img src={logo}></img></div>
             </div>
         </div>
     );
