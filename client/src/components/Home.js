261ba48fbecdaae54c2012ba6afe5173ce33c809
laponte16 <54826992+laponte16@users.noreply.github.com>
2023-02-07 17:47:40 -0400

    Update Home.js

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -17,11 +17,11 @@ function Home() {
             <div className='w-full fondo-gray mt-8 rounded pt-1 pb-8 pl-4 texto-white'>
                 <p className='texto-subtitulo'><b>Who We Are</b></p>
                 
-                <p className='w-4/5 mt-1'>Welcome all!</p>
+                <p className='w-4/5 mt-2'>Welcome all!</p>
                 <br></br>
-                <p>Thunderbit Code is an independent group of Computer Engineers specialized in Web Development, offering services ranging from simple static pages to the most state-of-the-art Apps. We're passionate about our work and trying out new things everyday, firmly bilinguals in both English and Spanish and fielding full fledged modern design and marketing strategies.</p>
+                <p className='w-4/5'>Thunderbit Code is an independent group of Computer Engineers specialized in Web Development, offering services ranging from simple static pages to the most state-of-the-art Apps. We're passionate about our work and trying out new things everyday, firmly bilinguals in both English and Spanish and fielding full fledged modern design and marketing strategies.</p>
                 <br></br>
-                <p>If you're starting a new business, branching out to new industries, modernizing your brand or simply want a little piece of the internet to build your dreams on? We would be more than happy to work with you together on achieving it.</p>
+                <p className='w-4/5'>If you're starting a new business, branching out to new industries, modernizing your brand or simply want a little piece of the internet to build your dreams on? We would be more than happy to work with you together on achieving it.</p>
             </div>
             <div className='flex flex-row justify-center w-full mt-10 px-16'>
                 <div className='flex flex-col justify-end w-1/4 p-2'><img src={logo}></img></div>
