782e1f3eb9495d3fff3cad1a2e813cc2c5e24ae5
mcastelli88 <mcastelliromero@gmail.com>
2023-02-07 18:17:57 -0400

    img

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,6 +1,10 @@
 import React from 'react'; // ES6 js
 import {Link} from 'react-router-dom';
-import logo from './images/logo.png'
+import logo from './images/logo.png';
+import home from './images/home.png';
+import portfolio from './images/portfolio.png';
+import services from './images/services.png';
+import contact from './images/contact.png';
 
 function Nav() {
     return(
@@ -9,11 +13,11 @@ function Nav() {
                 <img src={logo} className='w-1/5 p-2'></img>
                 <div className='items-end'>Redes sociales</div>
             </div>
-            <div className='flex flex-row justify-center texto-white'>
-                <Link to='/' className="pr-4">Home</Link>
-                <Link to='/portfolio' className="pr-4">Portfolio</Link>
-                <Link to='/services' className="pr-4">Services</Link>
-                <Link to='/contact' className="">Contact</Link>
+            <div className='flex flex-row w-full justify-between texto-white '>
+                <Link to='/' className="contents"><img className='p-0 m-0 w-1/12' src={home}></img></Link>
+                <Link to='/portfolio' className="p-0 m-0 w-1/12"><img src={portfolio}></img></Link>
+                <Link to='/services' className="p-0 m-0 w-1/12"><img src={services}></img></Link>
+                <Link to='/contact' className="p-0 m-0 w-1/12"><img src={contact}></img></Link>
             </div>
         </nav>
     );
