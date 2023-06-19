13807de83d17e061ee3dac68786479f6b9a562c5
ThunderbitCode <luismjaponte@gmail.com>
2023-06-19 13:02:57 -0400

    1

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -14,14 +14,26 @@ function Home() {
                     <Link to='/' className="texto-subtitulo fondo-blue w-fit p-1 rounded texto-white"><b>Our Offers</b></Link>
                 </div>
             </div>
-            <div className='w-full fondo-gray mt-8 rounded pt-1 pb-8 pl-4 texto-white'>
-                <p className='texto-subtitulo'><b>Who We Are</b></p>
+            <div className='w-full flex flex-col mt-8 rounded pt-1 pb-8 pl-4 texto-white'>
+                <p className='self-center texto-subtitulo fondo-gray p-1 rounded-md w-2/3 text-center'><b>Who We Are</b></p>
                 
-                <p className='w-4/5 mt-2'>Welcome all!</p>
                 <br></br>
-                <p className='w-4/5'>Thunderbit Code is an independent group of Computer Engineers specialized in Web Development, offering services ranging from simple static pages to the most state-of-the-art Apps. We're passionate about our work and trying out new things everyday, firmly bilinguals in both English and Spanish and fielding full fledged modern design and marketing strategies.</p>
+                <p className='self-center mt-3 fondo-gray p-1 rounded-md w-2/3 text-center'>
+                    Thunderbit Code is an independent group of Computer Engineers 
+                    specialized in Web Development, offering services 
+                    ranging from simple static pages to the most state-of-the-art Apps. 
+                    We're passionate about our work and trying out new things everyday, 
+                    firmly bilinguals in both English and Spanish 
+                    and fielding full fledged modern design and marketing strategies.
+                </p>
                 <br></br>
-                <p className='w-4/5'>If you're starting a new business, branching out to new industries, modernizing your brand or simply want a little piece of the internet to build your dreams on? We would be more than happy to work with you together on achieving it.</p>
+                <p className='self-center mt-3 fondo-gray p-1 rounded-md w-2/3 text-center'>
+                    If you're starting a new business, 
+                    branching out to new industries, 
+                    modernizing your brand or 
+                    simply want a little piece of the internet to build your dreams on? 
+                    We would be more than happy to work with you together on achieving it.
+                </p>
             </div>
             <div className='flex flex-row justify-center w-full mt-10 px-16'>
                 <div className='flex flex-col justify-end w-1/4 p-2'><img src={logo}></img></div>
