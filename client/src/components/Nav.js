91d30c454f262dbacf73f90993b87d179bce952a
mcastelli88 <mcastelliromero@gmail.com>
2023-06-19 17:38:35 -0400

    1

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,10 +1,7 @@
 import React from 'react'; // ES6 js
 import {Link} from 'react-router-dom';
 import logo from './images/logo.png';
-import home from './images/home.png';
-import portfolio from './images/portfolio.png';
-import services from './images/services.png';
-import contact from './images/contact.png';
+import { ImHome2, ImEnvelop, ImBriefcase, ImPower } from "react-icons/im";
 
 function Nav() {
     return(
@@ -16,10 +13,10 @@ function Nav() {
                 <div className='flex flex-row'>
                     <img src={logo} className='w-1/5 p-2'></img>
                     <div className='flex flex-row w-full justify-between texto-white '>
-                        <Link to='/' className="contents"><img className='p-0 m-0 w-1/12' src={home}></img></Link>
-                        <Link to='/portfolio' className="p-0 m-0 w-1/12"><img src={portfolio}></img></Link>
-                        <Link to='/services' className="p-0 m-0 w-1/12"><img src={services}></img></Link>
-                        <Link to='/contact' className="p-0 m-0 w-1/12"><img src={contact}></img></Link>
+                        <Link to='/' className="contents"><span className='w-fit h-fit text'><ImHome2/></span></Link>
+                        <Link to='/portfolio' ><span className=" w-fit h-fit"><ImBriefcase/></span></Link>
+                        <Link to='/services' ><span className="w-fit h-fit"><ImPower/></span></Link>
+                        <Link to='/contact' ><span className=" w-fit h-fit"><ImEnvelop/></span></Link>
                     </div>
                 </div>
             </div>
