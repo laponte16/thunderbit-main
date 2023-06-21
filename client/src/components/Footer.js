ecf713bbedc7375613674045fc84628dcdcd60a2
mcastelli88 <mcastelliromero@gmail.com>
2023-06-21 15:52:18 -0400

    1

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,11 +1,37 @@
 import React from 'react'; // ES6 js
 import {Link} from 'react-router-dom';
+import { ImTwitter, ImInstagram } from "react-icons/im";
+import { TbBrandFiverr, TbBrandUpwork } from "react-icons/tb";
 
 function Footer() {
     return(
         <footer className='fondo-blue'>
-            Footer
-            <div className='self-end'>Redes sociales</div>
+            <div className='flex flex-row w-full justify-start texto-white'>
+                <p>ThunderBit Kiss kiss muah muah</p>
+            </div>
+
+            <div className='flex flex-row w-full justify-end texto-white'>
+                <div className='p-2'>
+                    <Link to='/' className="w-fit h-fit">
+                        <span className=' texto-icon hover-text-blue'><ImTwitter/></span>
+                    </Link>
+                </div>
+                <div className='p-2'>
+                    <Link to='/portfolio' className="w-fit h-fit">
+                        <span className="texto-icon hover-text-blue"><ImInstagram/></span>
+                    </Link>
+                </div>
+                <div className='p-2'>
+                    <Link to='/services' className="w-fit h-fit">
+                        <span className=" texto-icon hover-text-blue"><TbBrandFiverr/></span>
+                    </Link>
+                </div>
+                <div className='p-2'>
+                    <Link to='/contact' className="w-fit h-fit">
+                        <span className=" texto-icon hover-text-blue"><TbBrandUpwork/></span>
+                    </Link>
+                </div>
+            </div>
         </footer>
     );
 }
