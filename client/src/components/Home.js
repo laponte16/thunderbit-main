649f1d37a5768fbf0c879d5f49777c6c24fae588
ThunderbitCode <luismjaponte@gmail.com>
2023-07-18 18:24:08 -0400

    1

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,6 +5,7 @@ import pexels from './images/pexels.gif'
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 import { ImPencil2, ImCogs, ImMobile, ImUser, ImLock } from "react-icons/im";
+import { Slide } from '@mui/material';
 
 function Home() {
     return(
@@ -22,51 +23,64 @@ function Home() {
                 </div>
             </div>
             <div className='w-full flex flex-col mt-8 rounded pt-1 pb-8 texto-white'>
-                <p className='self-center texto-big texto-blue overline p-1 rounded-md w-2/3 text-center'><b>Who We Are</b></p>
-
-                <p className='flex flex-col self-center mt-3 fondo-blue p-2 rounded-md w-2/3 text-center'>
-                    Thunderbit Code is an independent group of Computer Engineers 
-                    specialized in Web Development, offering services 
-                    ranging from simple static pages to the most state-of-the-art Apps. 
-                    We're passionate about our work and trying out new things everyday! 
+                <Slide in={true} direction={'right'} timeout={750}>
+                    <p className='self-center texto-big texto-blue overline p-1 rounded-md w-2/3 text-center'><b>Who We Are</b></p>
+                </Slide>
+                <Slide in={true} direction={'left'} timeout={750}>
+                    <p className='flex flex-col self-center mt-3 fondo-blue p-2 rounded-md w-2/3 text-center'>
+                        Thunderbit Code is an independent group of Computer Engineers 
+                        specialized in Web Development, offering services 
+                        ranging from simple static pages to the most state-of-the-art Apps. 
+                        We're passionate about our work and trying out new things everyday! 
+                        
+                        <span>
+                            Bilinguals in English and Spanish 
+                            and fielding fully fledged modern design and marketing strategies, 
+                            our team is ready to tackle any challenge.
+                        </span>
+                    </p>
+                </Slide>
+                <Slide in={true} direction={'right'} timeout={750}>
+                    <div className='flex flex-row justify-evenly texto-blue my-4 w-full sm:w-2/3 self-center text-center'>
+                        <div className='flex flex-col w-1/3 items-center'>
+                            <p><i><b>Design</b> </i></p>
+                            <span className='texto-icon mt-2'><ImPencil2/></span>
+                        </div>
+                        <div className='flex flex-col w-1/3 items-center'>
+                            <p><i><b>Performance</b> </i></p>
+                            <span className='texto-icon mt-2'><ImCogs/></span>
+                        </div>
+                        <div className='flex flex-col w-1/3 items-center'>
+                            <p><i><b>Multiplatform</b> </i></p>
+                            <span className='texto-icon mt-2'><ImMobile/></span>
+                        </div>
+                    </div>
+                </Slide>
+                <Slide in={true} direction={'left'} timeout={750}>
+                    <div className='flex flex-col justify-center'>
+                        <div className='flex flex-row justify-evenly texto-blue my-4 w-full sm:w-2/3 self-center text-center'>
+                            <div className='flex flex-col w-1/2 items-center'>
+                                <p><i><b>User Sessions</b> </i></p>
+                                <span className='texto-icon mt-2'><ImUser/></span>
+                            </div>
+                            <div className='flex flex-col w-1/2 items-center'>
+                                <p><i><b>Secure</b> </i></p>
+                                <span className='texto-icon mt-2 pl-3'><ImLock/></span>
+                            </div>
+                        </div>
+                        <p className='self-center mt-3 fondo-blue p-2 rounded-md w-2/3 text-center'>
+                            Are you starting a new business, 
+                            branching out to new sectors, 
+                            modernizing your brand or 
+                            simply want a little piece of the internet to build your dreams on? 
+                            We would be more than happy to work alongside you on achieving it.
+                        </p>
+                    </div>
                     
-                    <span>
-                        Bilinguals in English and Spanish 
-                        and fielding fully fledged modern design and marketing strategies, 
-                        our team is ready to tackle any challenge.
-                    </span>
-                </p>
-                <div className='flex flex-row justify-evenly texto-blue my-4 w-full sm:w-2/3 self-center text-center'>
-                    <div className='flex flex-col w-1/3 items-center'>
-                        <p><i><b>Design</b> </i></p>
-                        <span className='texto-icon mt-2'><ImPencil2/></span>
-                    </div>
-                    <div className='flex flex-col w-1/3 items-center'>
-                        <p><i><b>Performance</b> </i></p>
-                        <span className='texto-icon mt-2'><ImCogs/></span>
-                    </div>
-                    <div className='flex flex-col w-1/3 items-center'>
-                        <p><i><b>Multiplatform</b> </i></p>
-                        <span className='texto-icon mt-2'><ImMobile/></span>
-                    </div>
-                </div>
-                <div className='flex flex-row justify-evenly texto-blue my-4 w-full sm:w-2/3 self-center text-center'>
-                <div className='flex flex-col w-1/2 items-center'>
-                        <p><i><b>User Sessions</b> </i></p>
-                        <span className='texto-icon mt-2'><ImUser/></span>
-                    </div>
-                    <div className='flex flex-col w-1/2 items-center'>
-                        <p><i><b>Secure</b> </i></p>
-                        <span className='texto-icon mt-2 pl-3'><ImLock/></span>
-                    </div>
-                </div>
-                <p className='self-center mt-3 fondo-blue p-2 rounded-md w-2/3 text-center'>
-                    Are you starting a new business, 
-                    branching out to new sectors, 
-                    modernizing your brand or 
-                    simply want a little piece of the internet to build your dreams on? 
-                    We would be more than happy to work alongside you on achieving it.
-                </p>
+                </Slide>
+                
+                
+                
             </div>
             <Carousel
             showThumbs = {false}
